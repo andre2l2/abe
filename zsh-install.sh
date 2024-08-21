d717ca4fe4798d7dcdb646f7e1bbb2665f9bc82c
Andre Ferreira <delcferreira@gmail.com>
2024-08-20 22:46:57 -0300

    fix: update abe to suport bash

--- zsh-install.sh
+++ zsh-install.sh
@@ -5,15 +5,15 @@ abe_file_path="https://raw.githubusercontent.com/andre2l2/abe/master/src/abe"
 
 if [ -d $install_directory ]
 then
-  echo "Directory .abe is created!"
+  echo "Abe is installed!"
 else
   mkdir "$install_directory"
+
+  curl $abe_file_path > "$install_directory/abe"
+  chmod +x "$install_directory/abe"
+
+  echo "" >> "$HOME/.zshrc"
+  echo "export PATH="$PATH:$install_directory"" >> "$HOME/.zshrc"
+
+  echo "Abe installed!"
 fi
-
-cp $abe_file_path $install_directory
-chmod +x "$install_directory/abe"
-
-echo "" >> "$HOME/.zshrc"
-echo "export PATH="$PATH:$install_directory"" >> "$HOME/.zshrc"
-
-echo "Abe installed!"
